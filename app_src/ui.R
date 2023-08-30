f4d9da77bb6a99df6b20c6b0330ad247a64134d7
Nicolas Schlegel <schlegel.n@proton.me>
2023-08-30 11:54:53 +0200

    feature : adding text selector to chose the displayed year

--- app_src/ui.R
+++ app_src/ui.R
@@ -1,33 +1,20 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Coal World capacity visualisation"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+          textInput("year", "Year of the visualisation?")
+          ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+          textOutput("dataDisplay")
         )
     )
 )
