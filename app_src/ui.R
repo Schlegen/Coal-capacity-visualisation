7a8c12d3df008bd3632cbca89e693740f1d87254
Nicolas Schlegel <schlegel.n@proton.me>
2023-08-30 12:25:03 +0200

    feature : adding default value in text selector

--- app_src/ui.R
+++ app_src/ui.R
@@ -9,7 +9,7 @@ fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-          textInput("year", "Year of the visualisation?")
+          textInput("year", "Year of the visualisation?", value="1990")
           ),
 
         # Show a plot of the generated distribution
