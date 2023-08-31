3ab112a3badc279bfbb5d41e9128c42e9c465e37
Nicolas Schlegel <schlegel.n@proton.me>
2023-08-31 12:22:48 +0200

    BugFix : Use of the countrycode library for country name conversion to ISO3

--- app_src/server.R
+++ app_src/server.R
@@ -1,26 +1,23 @@
 library(shiny)
 library(readxl)
 library(dplyr)
-library(magrittr)
-library(rvest)
 # to import world map data
 library(maps)
 library(ggplot2)
 # for world map plot
 library(RColorBrewer)
 library(ggiraph)
+#For ISO3 country name conversion
+#Install with install.packages("countrycode") in a R console
+library(countrycode)
 
 PLANT_DATA_PATH = "data/Global-Coal-Plant-Tracker-July-2023.xlsx"
 PLANT_DATA <- read_excel(PLANT_DATA_PATH, sheet="Units")
-
-ISO_CODES_PATH <- "data/iso_3digit_alpha_country_codes.xls"
-ISO_CODES <- read_excel(ISO_CODES_PATH, skip=1)
-names(ISO_CODES) <- c("ISO3", "Country")
+PLANT_DATA["ISO3"] <- countrycode(PLANT_DATA$Country, origin = 'country.name', destination = 'iso3c') #read_excel(PLANT_DATA_PATH, sheet="Units")
 
 WORLD_DATA = ggplot2::map_data('world')
 WORLD_DATA <- fortify(WORLD_DATA)
-WORLD_DATA["ISO3"] <- ISO_CODES$ISO3[match(WORLD_DATA$region, ISO_CODES$Country)]
-
+WORLD_DATA["ISO3"] <- countrycode(WORLD_DATA$region, origin = 'country.name', destination = 'iso3c') #ISO_CODES$ISO3[match(WORLD_DATA$region, ISO_CODES$Country)]
 
 plot_theme <- function () { 
   theme_bw() + theme(axis.text=element_text(size = 14),
@@ -42,41 +39,41 @@ function(input, output, session) {
       #We first select the plants that operated during the year input$year and then sum them by country
       year_capacity_data <- PLANT_DATA[(PLANT_DATA$Status == 'operating' & PLANT_DATA$'Start year' <= observed_year) 
                                        | (PLANT_DATA$Status == 'retired' & PLANT_DATA$'Start year' <= observed_year & PLANT_DATA$'Retired year' >= input$year),] %>%
-                            group_by(Country)%>% summarise(coal_capacity=sum(`Capacity (MW)`), .groups='drop')
+                            group_by(ISO3)%>% summarise(coal_capacity=sum(`Capacity (MW)`), .groups='drop')
       
-      #merging with ISO_CODES data frame to get the right format for plots
-      year_capacity_data <- merge(x=ISO_CODES, y=year_capacity_data, by="Country",
-                                  all.x = TRUE) %>% select(ISO3, Country, coal_capacity)
-
+      #conversion of MW to GW
+      year_capacity_data["coal_capacity"] <- (10 ** (-3)) * year_capacity_data["coal_capacity"]
       year_capacity_data <- year_capacity_data[!is.na(year_capacity_data$ISO3), ]
-      
+
       # World map dataset for plots
       world_data <- WORLD_DATA
       world_data["coal_capacity"] <- year_capacity_data$coal_capacity[match(WORLD_DATA$"ISO3", year_capacity_data$"ISO3")]
       
-      
       #world map plot
       g <- ggplot() + 
         geom_polygon_interactive(data=world_data, color='gray70', size=0.1,
-                                 aes(x=long, y=lat, fill=coal_capacity, group=group, 
-                                 tooltip = sprintf("%s<br/>%s", ISO3, coal_capacity))) + 
-        scale_fill_gradientn(colours = brewer.pal(5, "RdBu"), na.value = 'white') + 
-        scale_y_continuous(limits = c(-60, 90), breaks = c()) + 
+                                 aes(x=long, y=lat, fill=coal_capacity, group=group,
+                                tooltip = sprintf("%s<br/>%s", ISO3, coal_capacity))) + 
+        scale_fill_gradientn(colours = brewer.pal(5, "RdBu"), na.value = 'gray80') + 
         scale_x_continuous(breaks = c()) +
-        labs(fill="Coal Power Plant Capacity", color="Coal Power Plant Capacity (MW)", title=NULL, x=NULL, y=NULL, caption=paste("Source: Global Energy Monitor - July 2023")) +
+        labs(fill="Coal Power Plant Capacity (GW)", color="Coal Power Plant Capacity (GW)", title=NULL, x=NULL, y=NULL, caption=paste("Source: Global Energy Monitor - July 2023")) +
         plot_theme()
-      
       
       return(g)
     })
     
     output$dataDisplay <- renderTable({
       observed_year <- as.numeric(input$year)
-      
-      #We plot the selected plants for debug
-      selected_plants <- PLANT_DATA[(PLANT_DATA$Status == 'operating' & PLANT_DATA$'Start year' <= observed_year) 
+      #We first select the plants that operated during the year input$year and then sum them by country
+      year_capacity_data <- PLANT_DATA[(PLANT_DATA$Status == 'operating' & PLANT_DATA$'Start year' <= observed_year) 
                                        | (PLANT_DATA$Status == 'retired' & PLANT_DATA$'Start year' <= observed_year & PLANT_DATA$'Retired year' >= input$year),] %>%
-                      select('Country', 'Status', 'Start year', 'Retired year')
-      head(selected_plants, 25)
+        group_by(Country, ISO3)%>% summarise(coal_capacity=sum(`Capacity (MW)`), .groups='drop')
+      #conversion of MW to GW
+      year_capacity_data["coal_capacity"] <- (10 ** (-3)) * year_capacity_data["coal_capacity"]
+
+      #merging with ISO_CODES data frame to get the right format for plots
+      year_capacity_data <- year_capacity_data %>% select(ISO3, Country, coal_capacity)
+      
+      year_capacity_data <- year_capacity_data[!is.na(year_capacity_data$ISO3), ]
     })
 }
