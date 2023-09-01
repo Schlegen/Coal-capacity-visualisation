7034cbd475e37f1a410eb32590153ef014d65e36
Nicolas Schlegel <schlegel.n@proton.me>
2023-09-01 08:33:20 +0200

    docs : correcting comments messages

--- app_src/server.R
+++ app_src/server.R
@@ -18,7 +18,7 @@ PLANT_DATA <- read_excel(PLANT_DATA_PATH, sheet="Units")
 #setting ISO3 country codes efficiently
 unique_plant_data_countries <- unique(PLANT_DATA$Country)
 suppressWarnings(#The following warning is about a country name (Kosovo) that was not converted to ISO3, it is treated manually
-iso3_lookup <- countrycode(unique_plant_data_countries, origin = 'country.name', destination = 'iso3c')
+iso3_lookup <- countrycode(unique_plant_data_countries, origin='country.name', destination='iso3c')
 )
 PLANT_DATA$ISO3 <- iso3_lookup[match(PLANT_DATA$Country, unique_plant_data_countries)]
 PLANT_DATA$ISO3[PLANT_DATA$Country == "Kosovo"] = "XXK"
@@ -28,13 +28,14 @@ WORLD_DATA = ggplot2::map_data('world')
 WORLD_DATA <- fortify(WORLD_DATA)
 unique_world_data_countries <- unique(WORLD_DATA$region)
 suppressWarnings(#The following warning is about country names (Kosovo and small islands) that was not converted to ISO3, it is treated manually (only Kosovo is needed, since Other countries are not in PLANT_DATA)
-iso3_lookup <- countrycode(unique_world_data_countries, origin = 'country.name', destination = 'iso3c')
+iso3_lookup <- countrycode(unique_world_data_countries, origin='country.name', destination='iso3c')
 )
 WORLD_DATA$ISO3 <- iso3_lookup[match(WORLD_DATA$region, unique_world_data_countries)]
 WORLD_DATA$ISO3[WORLD_DATA$region == "Kosovo"] = "XXK"
 
-
+#QUERY FUNCTION
 country_capacities <- function(observed_year) {
+  # We first select the plants that operated during the year input$year and then sum them by country
   year_capacity_data <- PLANT_DATA[(PLANT_DATA$Status == 'operating' & PLANT_DATA$'Start year' <= observed_year) 
                                    | (PLANT_DATA$Status == 'retired' & PLANT_DATA$'Start year' <= observed_year & PLANT_DATA$'Retired year' >= observed_year)
                                    | (PLANT_DATA$Status == 'mothballed' & PLANT_DATA$'Start year' <= observed_year),] %>%
@@ -43,37 +44,38 @@ country_capacities <- function(observed_year) {
   return(year_capacity_data)
 }
 
+# THEME FUNCTION
 plot_theme <- function () { 
-  theme_bw() + theme(axis.text=element_text(size = 14),
-                     axis.title=element_text(size = 14),
+  theme_bw() + theme(axis.text=element_text(size=14),
+                     axis.title=element_text(size=14),
                      axis.ticks.y=element_blank(),
                      axis.ticks.x=element_blank(),
                      axis.text.y=element_blank(),
                      axis.text.x=element_blank(),
-                     strip.text=element_text(size = 14),
+                     strip.text=element_text(size=14),
                      panel.grid.major=element_blank(), 
                      panel.grid.minor=element_blank(),
                      panel.background=element_blank(), 
                      legend.position="bottom",
                      panel.border=element_blank(), 
-                     strip.background=element_rect(fill = 'white', colour = 'white'))
+                     strip.background=element_rect(fill='white', colour='white'))
 }
 
+# PLOT FUNCTION
 plot_world_map <- function (observed_year) {
-  #We first select the plants that operated during the year input$year and then sum them by country
   year_capacity_data <- country_capacities(observed_year)
   
-  #conversion of MW to GW
+  #Conversion of MW to GW
   year_capacity_data$coal_capacity <- (10 ** (-3)) * year_capacity_data$coal_capacity
 
-  # World map dataset for plots
+  # World map data set for plots
   world_data <- WORLD_DATA
   world_data["coal_capacity"] <- year_capacity_data$coal_capacity[match(WORLD_DATA$ISO3, year_capacity_data$ISO3)]
   
   # Replacing Missing values by 0, assessing the database is comprehensive
   world_data[is.na(world_data$coal_capacity), "coal_capacity"] <- 0
   
-  #world map plot
+  # World map plot
   g <- ggplot() + 
     geom_polygon_interactive(data=world_data, color='gray70', size=0.1,
                              aes(x=long, y=lat, fill=coal_capacity, group=group,
--- app_src/ui.R
+++ app_src/ui.R
@@ -6,16 +6,15 @@ fluidPage(
     # Application title
     titlePanel("Coal Power Capacity per Country"),
 
-    # Sidebar with a slider input for number of bins
+    # Text input to select the year
     sidebarLayout(
         sidebarPanel(
           textInput("year", "Enter the year of the visualisation :", value="1990")
           ),
-
-        # Show a plot of the generated distribution
+        
+        # Map output
         mainPanel(
-          girafeOutput("worldPlot"),
-          tableOutput("dataDisplay")
+          girafeOutput("worldPlot")
         )
     )
 )
